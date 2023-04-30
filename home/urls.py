b58b224fc6c977b68c6e638543ca26ee710cab60
Jayant Raj <jayantraj66041@gmail.com>
2023-04-30 22:42:56 +0530

    home, profile, login layout

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from home.views import HomeView, SaloonDetailsView, LoginSignUpView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='index')
+    path('', HomeView.as_view(), name='index'),
+    path('saloon/', SaloonDetailsView.as_view(), name="saloon"),
+    path('login-signup/', LoginSignUpView.as_view(), name="login-signup"),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,15 @@ class HomeView(View):
     template_name = 'home/index.html'
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+    
+class SaloonDetailsView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "home/saloon.html", {
+
+        })
+    
+class LoginSignUpView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "home/login-signup.html", {
+            
+        })
