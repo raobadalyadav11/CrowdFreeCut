7083e70d03d15678681cb220a039434e9f713bd5
Jayant Raj <jayantraj66041@gmail.com>
2023-05-03 01:09:59 +0530

    dashboard data called

--- salon/urls.py
+++ salon/urls.py
@@ -3,6 +3,7 @@ from salon.views import Dashboard, CreateSalonProfile, DashboardFunctionView
 
 urlpatterns = [
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
-    path("dashboard/<str:msg>/", DashboardFunctionView.as_view(), name="dashboard-post"),
+    path('dashboard/<str:msg>/', Dashboard.as_view(), name='current-card-function'),
+    path("dashboard-function/<str:msg>/", DashboardFunctionView.as_view(), name="dashboard-post"),
     path("create-salon-profile/", CreateSalonProfile.as_view(), name="create-salon-profile"),
 ]
